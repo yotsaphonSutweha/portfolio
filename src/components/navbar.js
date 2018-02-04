9cbf91498f3738865b3c4b352982c43d8164201f
oofdon2540 <komsan_58@hotmail.com>
2018-02-04 01:16:57 +0000

    fixed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,20 +2,24 @@ import React from 'react';
 
 const NavBar = props => {
     return (
+      
+      <div className="navbar-fixed">
       <nav>
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo right">
-              <i className="large material-icons">insert_face</i>
-            </a>
+           
+            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
               <li><a href="#about">About</a></li>
               <li><a href="badges.html">Portfolio</a></li>
               <li><a href="collapsible.html">Work I do</a></li>
               <li><a href="collapsible.html">Contact</a></li>
             </ul>
+       
+       
+          
           </div>
       </nav>
-      
+    </div>
     );
 };
 
